68250d6fc83b535ba98050699e19c489af4c7bcd
Conor Shortt <conorshortt123@gmail.com>
2021-02-16 17:38:12 +0000

    Finished Lab: "Reviewing bit operations"

--- bitoperations.c
+++ bitoperations.c
@@ -1,30 +1,37 @@
 #include <stdio.h>
 
-void bin_print(int i) {
+void bin_print(unsigned int i) {
     // Number of bits in an int
-    int j = sizeof(int) * 8;
+    int j = sizeof(unsigned int) * 8;
 
     // Temp var
     int k;
 
-    // Figure out what this does: & = bitwise AND, ? = Ternary operator, like an IF statement.
+    // Loop over the number of bits in i (4 * 8)
     for(j-- ;j >= 0; j--) {
+        // 
         k = ((1 << j) & i) ? 1 : 0;
+        // Print k
         printf("%d", k);
     }
 }
 
 int main(int argc, char *argv[]) {
 
-    int i = 241;
+    // Set i to a literal value, Hex value of 241.
+    unsigned int i = 0xffffffff;
 
+    // Print original value in binary.
     printf("Original:  ");
     bin_print(i);
-    printf("\n");
+    printf("\t%x\t%u\n", i, i);
 
     for(int j = 0; j < 40; j++) {
-        printf("%3d << %2d: ", i, j);
+        // What the operation is.
+        printf("%3u << %2d: ", i, j);
+        // i in binary shifted left j times.
         bin_print(i << j);
+        // End line
         printf("\n");
     }
 
