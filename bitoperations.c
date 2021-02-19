cc5da8f437f3e208ce1196b938be885af3de605b
Conor Shortt <conorshortt123@gmail.com>
2021-02-19 15:05:52 +0000

    Completed ROTR function

--- bitoperations.c
+++ bitoperations.c
@@ -55,7 +55,7 @@ void Ch(int64_t x, int64_t y, int64_t z)
 
 // Logical function used in Secure Hash Algorithms, where x, y and z and 64-bit integers.
 // Referencing: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
-void Maj(int64_t x, int64_t y, int64_t z) 
+void Maj(int64_t x, int64_t y, int64_t z)
 {
     // Formula = Maj(x, y, z) = (x & y) ^ (x & z) ^ (y & z)
 
@@ -82,6 +82,35 @@ void Maj(int64_t x, int64_t y, int64_t z)
     }
 
     printf("\n");
+}
+
+// The rotate right (circular right shift) operation, where x is a w-bit word
+// and n is an integer (number of rotations) with 0 <= n < w
+// Referencing: https://nvlpubs.nist.gov/nistpubs/FIPS/NIST.FIPS.180-4.pdf
+void ROTR(int n, int64_t x)
+{
+    // Formula = ROTR^n(x) = (x >> n) | (x << w - n).
+
+    printf("\nROTR function: a logical function used in secure hash algorithms:\n");
+    printf("Given input (x, n) and evaluating with ROTR^n(x) = (x >> n) | (x << w - n)\n");
+    printf("Where w is the number of bits in x, and n is the number of rotations\n\n");
+
+    // Get ROTR(x)
+    for(int i = 0; i <= n; i++)
+    {   
+        int64_t rotr_x = (x >> i) | (x << (sizeof(x) * 8) - i);
+
+        // Print binary of ROTR(x)
+        int j = sizeof(int64_t) * 8;
+        for (j--; j >= 0; j--)
+        {
+            // Picks out the j^th bit of i, evaluates as true if it's a 1 else evaluates as 0.
+            int k = ((1 << j) & rotr_x) ? 1 : 0;
+            // Print k
+            printf("%d", k);
+        }
+        printf("\n");
+    }
 }
 
 int main(int argc, char *argv[])
@@ -112,12 +141,19 @@ int main(int argc, char *argv[])
     //     printf("\n\n");
     // }
 
-    int64_t x = 0x1234567891011121LL;
+    int64_t x = 0x1234F6789C011121LL;
     int64_t y = 0x9586912385591238LL;
     int64_t z = 0x1293756091714057LL;
 
+    printf("\nx = \t0x%" PRIX64 "\n", x);
+    printf("y = \t0x%" PRIX64 "\n", y);
+    printf("z = \t0x%" PRIX64 "\n", z);
+
     Ch(x, y, z);
     Maj(x, y, z);
+
+    ROTR(16, x);
+
     printf("\n");
 
     return 0;
