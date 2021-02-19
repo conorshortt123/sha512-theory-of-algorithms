b26c0a1c8d189df9516a5a68cc099c2a64d534d0
Conor Shortt <conorshortt123@gmail.com>
2021-02-19 14:00:28 +0000

    Implemented Ch function

--- bitoperations.c
+++ bitoperations.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <inttypes.h>
 
 void bin_print(unsigned int i)
 {
@@ -18,31 +19,74 @@ void bin_print(unsigned int i)
     }
 }
 
-int main(int argc, char *argv[])
+// Logical function used in Secure Hash Algorithms, where x, y and z and 64-bit integers.
+void Ch(int64_t x, int64_t y, int64_t z)
 {
+    // Formula = Ch(x, y, z) = (x & y) ^ (~x & z)
+    // & = Bitwise AND operator
+    // ^ = Bitwise XOR operator (exclusive OR)
+    // ~ = Bitwise NOT operator (inverts all bits)
 
-    // Set i to a literal value, Hex value of 241.
-    unsigned int i = 0x0f0f0f0f;
+    printf("\nCh function: a logical function used in secure hash algorithms:\n");
+    printf("Given input (x, y, z) and evaluating with Ch = (x & y) ^ (~x & z)\n");
+    printf("Where x, y and z are 64-bit numbers and output is also a 64-bit number\n\n");
 
-    // Print original value in binary.
-    printf("Original:\t");
-    bin_print(i);
+    int64_t ch = (x & y) ^ (~x & z);
 
-    printf("\t%x\t%u\n\n", i, i);
+    printf("In decimal:\t%" PRId64 "\n", ch);
+    printf("In hexidecimal:\t0x%" PRIX64 "\n", ch);
 
-    // 32
-    int j = sizeof(unsigned int) * 8;
+    // Size of 64-bit int
+    int j = sizeof(int64_t) * 8;
 
+    // Temp var
+    int k;
+
+    printf("In binary:\t");
+    // Loop over the number of bits in i (4 * 8)
     for (j--; j >= 0; j--)
     {
-        // 1 shifted left j times.
-        bin_print(1 << j); printf("\n");
-        // i.
-        bin_print(i); printf("\n");
-        printf("-------------------------------- &\n");
-        // (1 shifted left j times) bitwise logical and i.
-        bin_print((1 << j) & i); printf("\n\n");
+        // Picks out the j^th bit of i, evaluates as true if it's a 1 else evaluates as 0.
+        k = ((1 << j) & ch) ? 1 : 0;
+        // Print k
+        printf("%d", k);
     }
+}
+
+int main(int argc, char *argv[])
+{
+    // // Set i to a literal value, Hex value of 241.
+    // unsigned int i = 0x0f0f0f0f;
+
+    // // Print original value in binary.
+    // printf("Original:\t");
+    // bin_print(i);
+
+    // printf("\t%x\t%u\n\n", i, i);
+
+    // // 32
+    // int j = sizeof(unsigned int) * 8;
+
+    // for (j--; j >= 0; j--)
+    // {
+    //     // 1 shifted left j times.
+    //     bin_print(1 << j);
+    //     printf("\n");
+    //     // i.
+    //     bin_print(i);
+    //     printf("\n");
+    //     printf("-------------------------------- &\n");
+    //     // (1 shifted left j times) bitwise logical and i.
+    //     bin_print((1 << j) & i);
+    //     printf("\n\n");
+    // }
+
+    int64_t x = 0x0000444400004444LL;
+    int64_t y = 0x0000555500005555LL;
+    int64_t z = 0x0000666600006666LL;
+
+    Ch(x, y, z);
+    printf("\n");
 
     return 0;
 }
