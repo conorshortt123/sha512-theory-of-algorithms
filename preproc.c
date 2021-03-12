408deee30f9f35f51d3e3db8e9199dabe6fd3e56
Conor Shortt <conorshortt123@gmail.com>
2021-03-12 15:52:41 +0000

    Finished union lab, reads bytes and prints 32-bit words

--- preproc.c
+++ preproc.c
@@ -1,46 +1,68 @@
 #include <stdio.h>
 #include <inttypes.h>
 
+// Defining constants, WORD is a 32-bit integer value, PF is a constant
+// for the formatted printing of 32-bit integers.
+#define WORD uint32_t
+#define PF PRIX32
+#define W 32
 #define BYTE uint8_t
 
-void bin_print(BYTE i)
+union Block
 {
-    // Number of bits in an int
-    int j = sizeof(BYTE) * 8;
-
-    // Temp var
-    int k;
-
-    // Loop over the number of bits in i (4 * 8)
-    for (j--; j >= 0; j--)
-    {
-        // Picks out the j^th bit of i, evaluates as true if it's a 1 else evaluates as 0.
-        k = ((1 << j) & i) ? 1 : 0;
-        // Print k
-        printf("%d", k);
-    }
-}
+    BYTE bytes[64];
+    WORD words[16];
+};
 
 int main(int argc, char *argv[])
 {
-    BYTE b;
+    union Block B;
 
-    // Project: error checks
+    // Total number of bits read
+    uint64_t nobits = 0;
+    // Iterator
+    int i;
+
+    // File pointer for reading - Project: error checks
     FILE *f;
-    // Project: verify args
+    // Open from command line for reading - Project: verify args
     f = fopen(argv[1], "rb");
 
+    // Number of bytes to read
     size_t numbytes;
 
-    numbytes = fread(&b, 1, 1, f);
-    while (numbytes)
+    // Try to read 64 bytes
+    numbytes = fread(B.bytes, 1, 64, f);
+    // Print to console how many bytes were read.
+    printf("Read %d bytes.\n", numbytes);
+    // Update number of bits read
+    nobits = nobits + (8 * numbytes);
+    // Print the 16 32-bit words.
+    for (i = 0; i < 16; i++)
     {
-        bin_print(b);
-        numbytes = fread(&b, 1, 1, f);
+        printf("%08" PF " ", B.words[i]);
+        if ((i + 1) % 8 == 0)
+            printf("\n");
     }
+
+    while (!feof(f))
+    {
+        numbytes = fread(B.bytes, 1, 64, f);
+        printf("Read %d bytes\n", numbytes);
+        nobits = nobits + (8 * numbytes);
+
+        // Print the 16 32-bit words.
+        for (i = 0; i < 16; i++)
+        {
+            printf("%08" PF " ", B.words[i]);
+            if ((i + 1) % 8 == 0)
+                printf("\n");
+        }
+    }
+    // Close file.
     fclose(f);
     printf("\n");
-    
+    printf("Total bits read is %d\n", nobits);
 
     return 0;
 }
